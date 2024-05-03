fd3514b83d671f78f18e111051a90a2956f8baaa
Masoomyar34 <155780455+Masoomyar34@users.noreply.github.com>
2024-05-04 02:32:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ import moment from "moment-timezone"
 
 
 
-global.owner = [
-  ["923092668108", "Prince", false],
+SADAMO.owner = [
+  ["923167437883", "SADAM HUSSAIN", true],
   [''], 
   [''] 
 ]
@@ -20,7 +20,7 @@ global.owner = [
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+global.mods = ['+263 71 148 7465] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -59,7 +59,7 @@ global.APIKeys = { // APIKey Here
 
 
 
-// Bot Images 
+// Bot Images https://telegra.ph/file/cc16c74000397e17e57fb.jpg
 global.imagen1 = fs.readFileSync("./Assets/menus/Menu.png")
 global.imagen2 = fs.readFileSync("./Assets/menus/Menu1.jpg")
 global.imagen3 = fs.readFileSync("./Assets/menus/Menu2.jpg")
@@ -120,12 +120,12 @@ global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
 
 
 //Sticker WM
-global.botname = '𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋'
-global.princebot = '🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️'
-global.packname = 'Prince♥️' 
-global.author = 'Prince♥️' 
-global.princeig = 'https://www.instagram.com' 
-global.princegp = 'https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS'
+global.botname = 'SADAMO'
+global.princebot = '🛡️SADAMO🛡️'
+global.packname = 'SADAMO♥️' 
+global.author = 'SADAMO♥️' 
+global.princeig = '' 
+global.princegp = 'https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS
 global.menuvid = 'https://i.imgur.com/GFAAXqw.mp4'
 global.Princesc = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT' 
 global.princeyt = 'https://youtube.com/'
